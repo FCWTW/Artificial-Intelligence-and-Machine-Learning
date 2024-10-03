04baac44e7d5b5a9464f4da622be0455b391d3a0
Wayne <waynetw1223@gmail.com>
2024-10-03 16:34:14 +0800

    update flip coin

---
+++ flip_coin/README.md
@@ -0,0 +1,12 @@
+# An example of Bayesian Decision Theory
+
+## Event explanation in code:
+* C 代表 "在投擲硬幣之前，認為硬幣是否 biased "
+* x 代表 "出現 num 次正面且總共投擲 10 次"
+
+![Ba](/Image/Bayes.png)
+
+> <details>
+>  <summary>Reference</summary>
+>    * <a href="https://numustafa.medium.com/basics-of-machine-learning-understanding-bayesian-decision-theory-eb54ed405">Basics of Machine Learning: Understanding Bayesian Decision Theory</a><br>
+> </details>
---
+++ flip_coin/flip_coin.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# define prior probabilities: p(C)
+C0 = 0.4
+C1 = 0.6
+# This means we assume the coin has a 60% chance of being biased, and has a 40% chance of being fair.
+# We also suppose that a biased coin has an 80% chance of flipping to heads. (that is why line 30 has a 0.8)
+
+# the number of heads
+num = 7
+
+# define likelihood function: p(x|C)
+def likelihood(num, theta):
+    return theta**num * (1-theta)**(10-num)
+
+# calculate posterior probabilities: p(C|x)
+def posterior(num, C0, C1):
+    likelihood_0 = likelihood(num, 0.5)
+    likelihood_1 = likelihood(num, 0.8)
+    numerator_0 = likelihood_0 * C0
+    numerator_1 = likelihood_1 * C1
+
+    # p(x) = p(x|C=1)*p(C=1) + p(x|C=0)*p(C=0)
+    evidence = numerator_1 + numerator_0
+    
+    # p(C|x) = p(C)*p(x|C) / p(x)
+    posterior_0 = numerator_0 / evidence
+    posterior_1 = numerator_1 / evidence
+    return posterior_0, posterior_1
+
+if __name__ == "__main__":
+    posterior_0, posterior_1 = posterior(num, C0, C1)
+    print(f"Posterior probability of fair coin: {posterior_0:.3f}")
+    print(f"Posterior probability of biased coin: {posterior_1:.3f}")
+
+    # plot classifier
+    x_values = np.arange(11)
+    posterior_1_values = [posterior(x, C0, C1)[1] for x in x_values]
+    plt.plot(x_values, posterior_1_values, label="Posterior probability of biased coin")
+    plt.axhline(y=0.5, color="r", linestyle="--", label="Decision boundary")
+    plt.xlabel("Number of heads")
+    plt.ylabel("Posterior probability")
+    plt.legend()
+    plt.show()
