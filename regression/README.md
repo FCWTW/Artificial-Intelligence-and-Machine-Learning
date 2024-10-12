ef11f58f30a90a71401dc6f668bce1df489fa1f7
Wayne <waynetw1223@gmail.com>
2024-10-12 20:35:08 +0800

    update linear regression

---
+++ regression/README.md
@@ -0,0 +1,7 @@
+# Linear Regression Implementation
+
+> <details>
+>  <summary>Reference</summary>
+> 1. <a href=""></a><br>
+> 2. <a href="https://medium.com/qiubingcheng/%E5%9B%9E%E6%AD%B8%E5%88%86%E6%9E%90-regression-analysis-%E7%9A%84r%E5%B9%B3%E6%96%B9-r-squared-%E8%88%87%E8%AA%BF%E6%95%B4%E5%BE%8Cr%E5%B9%B3%E6%96%B9-adjusted-r-squared-f38ad733bc4e">回歸分析的R平方s與調整後R平方</a><br>
+> </details>
---
+++ regression/linear_regression.py
@@ -0,0 +1,74 @@
+import numpy as np
+import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# dataset path
+path = "dataset/classification_data.csv"
+
+def generate_csv(X, y):
+    directory = os.path.dirname(path)
+    y_series = pd.Series(y)
+
+    # reset index
+    y_series.reset_index(drop=True, inplace=True)
+    X.reset_index(drop=True, inplace=True)
+
+    # combine test data and class label
+    X.insert(X.shape[1], column="class", value=y_series)
+    X.to_csv(os.path.join(directory+"/output.csv"), index=False)
+
+class Linear_regression():
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
+
+    def fit(self):
+        self.b = self.estimate_coef()
+        self.X, self.mean, self.std = self.standardize(self.X)
+    
+    def estimate_coef(self): 
+        X = np.c_[np.ones((self.X.shape[0], 1)), self.X]
+        X_transpose = X.T
+
+        # b = (X^T X)^(-1) X^T y
+        b = np.linalg.inv(X_transpose @ X) @ X_transpose @ self.y
+        return b
+    
+    def predict(self, testX):
+        std_testX = (testX - self.mean) / self.std
+        std_testX = np.c_[np.ones((std_testX.shape[0], 1)), std_testX]
+        return std_testX @ self.b
+    
+    def standardize(self, X):
+        mean = np.mean(X, axis=0)
+        std = np.std(X, axis=0)
+        return (X - mean)/std, mean, std
+    
+    def mean_squared_error(self, y_true, y_pred):
+        return np.mean((y_true - y_pred) ** 2)
+
+    def r2_score(self, y_true, y_pred):
+        total_variance = np.sum((y_true - np.mean(y_true)) ** 2)
+        residual_variance = np.sum((y_true - y_pred) ** 2)
+        return 1 - (residual_variance / total_variance)
+
+if __name__ == "__main__":
+    df = pd.read_csv(path)
+    X = df.iloc[:, :-1]
+    y = df.iloc[:, -1]
+
+    # split dataset and reshape for Gaussian_NB
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=0)
+    
+    Lr = Linear_regression(X_train, y_train)
+    Lr.fit()
+
+    y_pred = Lr.predict(X_test)
+    mse = Lr.mean_squared_error(y_test, y_pred)
+    r2 = Lr.r2_score(y_test, y_pred)
+    
+    print(f'Mean Squared Error: {mse}')
+    print(f'R² Score: {r2}')
+
+    generate_csv(X_test, y_pred)
